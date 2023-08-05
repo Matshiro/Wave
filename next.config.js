1b9970ba6b65ad631424c92538c340055fcb51d4
Matshiro <daniel.b.lewinski@gmail.com>
2023-08-05 19:51:00 +0200

    Finished user avatar and hardcoded online status

--- next.config.js
+++ next.config.js
@@ -3,6 +3,27 @@ const nextConfig = {
   experimental: {
     swcPlugins: [["next-superjson-plugin", {}]],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/u/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.cloudinary.com",
+        port: "",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
